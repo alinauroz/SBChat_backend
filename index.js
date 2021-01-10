eb7ecc6ddd7fd693de00a0b54992c564fede6a0e
MacBook <alinauroze@hotmail.com>
2021-01-10 17:43:36 +0500

    feat: sign in

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const io = require("socket.io")(server);
 const users = {};
 
 io.on('connection', (socket) => {
-
+    socket.on('join', (data, callback) => {
+        if (data.username in users) {
+            callback({msg: `User with this username already exists`});
+        }
+        else {
+            users[data.username] = data;
+            users[data.username].socket = socket;
+            callback(null, {msg: 'User joined successfully'});
+        }
+    })
 })
 
 app.get('/users', (req, res) => {
