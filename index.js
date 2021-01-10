a9b8c5d43e0aec834988fa64c163e9e81c1ab4d4
MacBook <alinauroze@hotmail.com>
2021-01-10 20:12:30 +0500

    save changes

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const users = {};
+const messages = [];
 
 io.on('connection', (socket) => {
+
     socket.on('join', (data, callback) => {
         if (data.username in users) {
             callback({msg: `User with this username already exists`});
         }
         else {
+            socket.username = data.username;
             users[data.username] = data;
             users[data.username].socket = socket;
             callback(null, {msg: 'User joined successfully'});
         }
+    })
+
+    socket.on('new_message', (data) => {
+        messages.push(data);
+        io.sockets.emit('new_message', data);
     })
 })
 
@@ -28,5 +36,9 @@ app.get('/users', (req, res) => {
     }
     res.send(_users)
 });
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
 
 server.listen(3000)
